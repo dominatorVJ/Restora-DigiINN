ef8be600ffaa7f39c86da2f6394c99a8f466dd6d
dominatorVJ <1vishaljee@gmail.com>
2023-12-24 18:17:05 +0530

    Initial Setup

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/**
+ * Wireframe/Design Mock
+ * -Header
+ *   -Logo
+ *   -Nav Items
+ * -Body
+ *   -Search input
+ *   -Restaurant Container
+ *     -Restaurant Card
+ *      -Food Name
+ *      -Category
+ *      -Rating
+ * -Footer
+ *   -Copyright
+ *   -Social Links
+ *   -Address
+ *   -Contacts
+ */
+
+const AppLayout = () => {};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render();
