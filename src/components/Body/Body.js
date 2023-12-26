fa288ef4af8126cc52e0160a7695e8ab4338d69b
dominatorVJ <1vishaljee@gmail.com>
2023-12-26 17:19:50 +0530

    feat:featur added - Top Rated Restaurant

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,14 +1,33 @@
 import RestaurantCard from "./RestaurantCard/RestaurantCard.js";
 import SearchBar from "./SearchBar/SearchBar.js";
 import resList from "/src/utils/RestaurantData.js";
+import { useState } from "react";
+import "./Filter.css"
+
 import "./Body.css";
 const Body = () => {
+  const [ResList, setResList] = useState(resList);
   return (
     <div className="body">
-      <SearchBar />
+      <div className="body-top">
+        {<SearchBar />}
+        <div className="filter">
+          <button
+            className="filter-btn"
+            onClick={() => {
+              const fresList = resList.filter(
+                (res) => res.data.avgRating > 4.0
+              );
+              setResList(fresList);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
-          <RestaurantCard key = {restaurant.data.id} resData={restaurant} />
+        {ResList.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
     </div>
