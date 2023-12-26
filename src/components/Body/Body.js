2b163c7214a3322fd87b0da028a916c068324d49
dominatorVJ <1vishaljee@gmail.com>
2023-12-26 23:59:38 +0530

    feat: dynamic restaurant card data - Integrated swiggy API

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,12 +1,29 @@
 import RestaurantCard from "./RestaurantCard/RestaurantCard.js";
 import SearchBar from "./SearchBar/SearchBar.js";
-import resList from "/src/utils/RestaurantData.js";
-import { useState } from "react";
-import "./Filter.css"
-
+import { useState, useEffect } from "react";
+import { API_URL } from "../../utils/constants.js";
+import "./Filter.css";
 import "./Body.css";
+import Shimmer from "./Shimmer.js";
+
 const Body = () => {
-  const [ResList, setResList] = useState(resList);
+  const [ResList, setResList] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(API_URL);
+    const json = await data.json();
+    //Optional Chaining
+    setResList(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (ResList.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div className="body">
       <div className="body-top">
@@ -15,8 +32,8 @@ const Body = () => {
           <button
             className="filter-btn"
             onClick={() => {
-              const fresList = resList.filter(
-                (res) => res.data.avgRating > 4.0
+              const fresList = ResList.filter(
+                (res) => res.info.avgRating > 4.0
               );
               setResList(fresList);
             }}
@@ -27,7 +44,7 @@ const Body = () => {
       </div>
       <div className="res-container">
         {ResList.map((restaurant) => (
-          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
--- src/components/Body/RestaurantCard/RestaurantCard.js
+++ src/components/Body/RestaurantCard/RestaurantCard.js
@@ -8,17 +8,17 @@ const RestaurantCard = (props) => {
         className="res-logo"
         src={
           CDN_URL +
-          resData.data.cloudinaryImageId
+          resData.info.cloudinaryImageId
         }
       />
-      <h3>{resData.data.name}</h3>
-      <h4>{resData.data.cuisines.join(", ")}</h4>
-      <h4>{resData.data.avgRating} stars</h4>
+      <h3>{resData.info.name}</h3>
+      <h4>{resData.info.cuisines.join(", ")}</h4>
+      <h4>{resData.info.avgRating} stars</h4>
       <h4>
-        {resData.data.lastMileTravelString}{" | "}
-        {resData.data.deliveryTime} minutes
+        {resData.info.sla.lastMileTravelString}{" | "}
+        {resData.info.sla.deliveryTime} minutes
       </h4>
-      <h4>{resData.data.costForTwoString}</h4>
+      <h4>{resData.info.costForTwoString}</h4>
     </div>
   );
 };
