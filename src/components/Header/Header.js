c67803c6c2e451ee62e9b6db12d49f83d5b6a4e4
dominatorVJ <1vishaljee@gmail.com>
2023-12-26 17:35:49 +0530

    style: headerborder and feat: logo click - refresh page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
 import logo from "/src/assets/logo.png";
 import "./Header.css"
+ function refreshPage() {
+  window.location.reload(false);
+}
 const Header = () => {
   return (
     <div className="header">
       <div className="logo-container">
-        <img className="logo" src={logo} />
+        <img
+          className="logo"
+          src={logo}
+          onClick={refreshPage}
+        />
       </div>
       <div className="nav-items">
         <ul>
