8ed7de4c154ce884aab610629ae914fde89a173e
dominatorVJ <1vishaljee@gmail.com>
2023-12-28 20:10:55 +0530

    feat: dummy login-logout button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,15 @@
 import logo from "/src/assets/logo.png";
-import "./Header.css"
- function refreshPage() {
+import "./Header.css";
+import { useState } from "react";
+function refreshPage() {
   window.location.reload(false);
 }
 const Header = () => {
+  const [btnName, setbtnName] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src={logo}
-          onClick={refreshPage}
-        />
+        <img className="logo" src={logo} onClick={refreshPage} />
       </div>
       <div className="nav-items">
         <ul>
@@ -19,6 +17,18 @@ const Header = () => {
           <li>About</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <li>
+            <button
+              className="login"
+              onClick={() => {
+                return btnName === "Logout"
+                  ? setbtnName("Login")
+                  : setbtnName("Logout");
+              }}
+            >
+              {btnName}
+            </button>
+          </li>
         </ul>
       </div>
     </div>
