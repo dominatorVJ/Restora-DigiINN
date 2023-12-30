9eb1dba642b3c46f8765632addc98859f27795a8
dominatorVJ <1vishaljee@gmail.com>
2023-12-30 19:41:52 +0530

    update: swiggy api uri updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@ export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const API_URL =
-  "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3699587&lng=82.9696458&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
